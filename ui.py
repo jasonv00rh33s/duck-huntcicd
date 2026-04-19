8631346ae492bf908f46be03cbdf687057f58b46
Markiyan Baziuk <markiyanbaziuk@knu.ua>
2026-04-19 13:41:04 +0300

    Створення функції для загрузки картинок

--- ui.py
+++ ui.py
@@ -9,3 +9,15 @@ class HUD:
         self.bg_color = bg_color
         self._load_assets()   
         self._init_crosshair() 
+
+        def _load_assets(self):
+        e = self.engine
+        self.background = e.load_image("assets/background.png")
+        self.overlay = e.load_image("assets/overlay.png")
+        self.score_img = e.load_image("assets/score.png")
+        self.red_cross = e.load_image("assets/red_cross.png")
+        self.crosshair = e.load_image("assets/crosshair.png")  
+        duck_raw = e.load_image("assets/duck.png")
+        w = duck_raw.get_width() // 3
+        h = duck_raw.get_height() // 3
+        self.duck_surface = pygame.transform.scale(duck_raw, (w, h))
