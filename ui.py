afa68f416d9d122b5f704f73f136f84378903b1f
Markiyan Baziuk <markiyanbaziuk@knu.ua>
2026-04-19 13:40:20 +0300

    Створення класу HUD і створення функції ініціалізції

--- ui.py
+++ ui.py
@@ -2,3 +2,10 @@ import pygame
 import sys
 from tkinter import messagebox, Tk
 from src.config import SCREEN_WIDTH
+
+class HUD:
+    def __init__(self, engine, bg_color="image"):
+        self.engine = engine
+        self.bg_color = bg_color
+        self._load_assets()   
+        self._init_crosshair() 
